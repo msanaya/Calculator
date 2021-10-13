b397943dacead0206e95b3a0946d0d1ec3f3ef62
Pilar-B <pbarkerit@gmail.com>
2021-10-13 16:34:04 -0400

    Updated js to work for addition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/// Business Logic ///
+
 function add(number1, number2) {
     return number1 + number2;
 }
@@ -14,11 +16,19 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const resultAdd = add(number1, number2);
-const resultSub = substract(number1, number2);
-const resultMul = multiply(number1, number2);
-const resultDiv = divide(number1, number2);
+/// Everything below this line is user interface logic //
 
-alert(resultDiv);
+$(document).ready(function () {
+    $("form#add").submit(function (event) {
+        event.preventDefault();
+        const number1 = parseInt($("#add1").val());
+        const number2 = parseInt($("#add2").val());
+        const resultAdd = add(number1, number2);
+        $("#output").text(resultAdd)
+    });
+});
+
+// const resultSub = substract(number1, number2);
+// const resultMul = multiply(number1, number2);
+// const resultDiv = divide(number1, number2);
+
